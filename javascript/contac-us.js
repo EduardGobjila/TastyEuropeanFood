4d6f40ee52d4db84ab6f5ec2495acfe503a8c20b
Eduard Gobjila <edikgobjila01@gmail.com>
2022-11-27 19:16:23 +0000

    added the javascript

--- javascript/contac-us.js
+++ javascript/contac-us.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+let contacts = document.getElementById('numbers')
+
+console.log(numbers)
+if (window.innerWidth < 520) {
+  contacts.innerHTML = `
+  <p>Shop Telephone Number: <br> <a href="tel:+442083431431">+44 20 8343 1431</a></p>
+  <p>Manager's Phone Number: <br> <a href="tel:+447464736368">+44 (74) 6473 6368</a></p>
+  <p>Delivery Driver Phone Number: <br> <a href="tel:+447981801403">+44 (79) 8180 1403</a></p>
+  <p>Orders Telephone Number: <br> <a href="tel:+442083431431">+44 20 8343 1431</a></p>
+  <p>Email: <br> <a href="mailto:tastyeuropean@gmail.com">tastyeuropean@gmail.com</a></p>
+  `
+}
