aab7e49c4fe414254a71112e021d939d40d724e3
Eduard Gobjila <edikgobjila01@gmail.com>
2022-12-01 18:29:31 +0000

    fixed javascript bug + added the routes to each category

--- javascript/coming_soon.js
+++ javascript/coming_soon.js
@@ -1,16 +1,16 @@
-let newCountDownDate = new Date("Dec 02 2022 15:37:25").getTime()
+let newCountDownDate = new Date("Jan 20 2023, 15:37:25").getTime()
 
-
+console.log(newCountDownDate)
 let timer = setInterval(() => {
 
   let timeNow = new Date().getTime()
 
-  let difference = newCountDownDate = timeNow
+  let difference = newCountDownDate - timeNow
 
   let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60 * 60))
-  let seconds = Math.floor((difference % (1000 * 60)) / (1000 *  60))
+  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / (1000 *  60));
 
 
   document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm '
