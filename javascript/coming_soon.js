50b362308c7acd7f0813f54de45ef32b075906e3
Eduard Gobjila <edikgobjila01@gmail.com>
2022-12-02 21:43:52 +0000

    added a countdown which already finished the logic

--- javascript/coming_soon.js
+++ javascript/coming_soon.js
@@ -1,16 +1,19 @@
 let newCountDownDate = new Date("Jan 20 2023, 15:37:25").getTime()
 
-console.log(newCountDownDate)
+
+
 let timer = setInterval(() => {
 
   let timeNow = new Date().getTime()
 
   let difference = newCountDownDate - timeNow
 
+
+  // console.log(difference)
   let days = Math.floor(difference / (1000 * 60 * 60 * 24));
   let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60 * 60));
-  let seconds = Math.floor((difference % (1000 * 60)) / (1000 *  60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / (1000));
 
 
   document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm '
